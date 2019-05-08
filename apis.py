f5f0ab478c21110166d5bb90120c56f3d0060f23
Lea Ellingwood <leaellingwood@hotmail.com>
2019-05-08 17:12:44 -0400

    added if language to update todos

--- apis.py
+++ apis.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 api = Api(app)
 
 TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
+    "todo1": {"task": "build an API"},
+    "todo2": {"task": "?????"},
+    "todo3": {"task": "profit!"},
 }
 
 
@@ -19,10 +19,10 @@ def abort_if_todo_doesnt_exist(todo_id):
 
 
 parser = reqparse.RequestParser()
-parser.add_argument('task')
-parser.add_argument('due date')
-parser.add_argument('completed')
-parser.add_argument('date completed')
+parser.add_argument("task")
+parser.add_argument("due date")
+parser.add_argument("completed")
+parser.add_argument("date completed")
 
 
 # Todo
@@ -35,19 +35,34 @@ class Todo(Resource):
     def delete(self, todo_id):
         abort_if_todo_doesnt_exist(todo_id)
         del TODOS[todo_id]
-        return '', 204
+        return "", 204
 
     def put(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
         args = parser.parse_args()
-        task = {'task title': args['task']}
-        due_date = {'due date': args['due date']}
-        last_updated = {'last upated': str(datetime.datetime.now())}
-        completed = {'completed': args['completed']}
-        completed_date = {'date completed': args['date completed']}
+        targeted_todo = TODOS[todo_id]
+        # targeted_todo.update({'task title': args['task'],'due date': args["due date"], "completed": args["completed"]})
+        if args.get('task'):
+            TODOS[todo_id].update({'task': args['task']})
+            
+        if args.get('due_date'):
+            TODOS[todo_id].upate({'due date': args['due_date']}
+
+        if args.get('completed'):
+            TODOS[todo_id].update({'completed': args['completed']}
+
+        task = {"task title": args["task"]}
+        due_date = {"due date": args["due date"]}
+        last_updated = {"last upated": str(datetime.datetime.now())}
+        completed = {"completed": args["completed"]}
+        completed_date = {"date completed": args["date completed"]}
         if completed and not completed_date:
-            completed_date = {'completed date': str(datetime.datetime.now())}
-        TODOS[todo_id] = task, last_updated, due_date, completed
+            completed_date = {"completed date": str(datetime.datetime.now())}
+        TODOS[todo_id] = task, last_updated, due_date, completed, completed_date
         return task, due_date, completed, 201
+
+
+# For your `put` function, you want to grab the existing todo (using `todo_id`) in your `TODOS` dict, and update the values inside of that object. Look into the update method for python dictionaries.
 
 
 # TodoList
@@ -58,18 +73,23 @@ class TodoList(Resource):
 
     def post(self):
         args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
+        todo_id = int(max(TODOS.keys()).lstrip("todo")) + 1
+        todo_id = "todo%i" % todo_id
         # creation_date = {'creation date': str(datetime.datetime.now())}
-        TODOS[todo_id] = {'task': args['task'], 'creation_date': str(datetime.datetime.now()), 'completed': args['completed'], 'due date': args['due date']}
+        TODOS[todo_id] = {
+            "task": args["task"],
+            "creation_date": str(datetime.datetime.now()),
+            "completed": args["completed"],
+            "due date": args["due date"],
+        }
         return TODOS[todo_id], 201
 
 
 ## Actually setup the Api resource routing here
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
+api.add_resource(TodoList, "/todos")
+api.add_resource(Todo, "/todos/<todo_id>")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
 # ```
